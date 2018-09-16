d3a3cbabc7a896dee2635dca419c729bc2ff9a2b
R. Stanley Hum <r.stanley.hum@gmail.com>
2018-09-16 19:53:15 -0400

    added basic firebase analytics to screens

--- lib/views/base_view.dart
+++ lib/views/base_view.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 import '../constants.dart' as globals;
@@ -10,11 +14,25 @@ import '../widgets/primary_bottom_bar.dart';
 import '../widgets/main_drawer.dart';
 
 class BaseView extends StatefulWidget {
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
+
+  BaseView({
+    this.analytics,
+    this.observer,
+  }) : super();
+
   @override
-  State<StatefulWidget> createState() => _BaseViewState();
+  State<StatefulWidget> createState() => _BaseViewState(
+        analytics: analytics,
+        observer: observer,
+      );
 }
 
 class _BaseViewState extends State<BaseView> {
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
+
   int _currentIndex;
   final List<Widget> children = [
     NewsViewport(),
@@ -22,18 +40,36 @@ class _BaseViewState extends State<BaseView> {
     PharmaViewport()
   ];
 
+  _BaseViewState({
+    this.analytics,
+    this.observer,
+  }) : super();
+
   @override
   void initState() {
     super.initState();
     setState(() {
       _currentIndex = globals.initialTabIndex;
+      _sendCurrentScreen().catchError((e) {
+        print("InitState: $e");
+      });
     });
   }
 
   void _onTabSelected(int index) {
     setState(() {
       _currentIndex = index;
+      _sendCurrentScreen().catchError((e) {
+        print(e);
+      });
     });
+  }
+
+  Future<Null> _sendCurrentScreen() async {
+    print("_sendCurrentScreen: $_currentIndex");
+    await analytics.setCurrentScreen(
+      screenName: "Index $_currentIndex",
+    );
   }
 
   @override
--- lib/views/material_base.dart
+++ lib/views/material_base.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 import '../constants.dart' as globals;
 
 import 'base_view.dart';
 
 class MaterialBase extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(
+    analytics: analytics,
+  );
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: globals.materialAppTitle,
         theme: ThemeData(
           primarySwatch: globals.themeColor,
         ),
-        home: BaseView(),
+        navigatorObservers: <NavigatorObserver>[observer],
+        home: BaseView(
+          analytics: analytics,
+          observer: observer,
+        ),
       );
 }
