8a2e45c22c2299a2a0082b061f0c26fb06b41f1c
R. Stanley Hum <r.stanley.hum@gmail.com>
2018-09-17 16:29:59 -0400

    added redux_thunk

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_logging/redux_logging.dart';
+import 'package:redux_thunk/redux_thunk.dart';
 
 import 'models/app_state.dart';
 import 'reducers/app_state_reducer.dart';
@@ -20,7 +21,10 @@ void main() {
       handbookArticles: assets.handbookList,
       pharmaArticles: assets.pharmaList,
     ),
-    middleware: [LoggingMiddleware.printer()],
+    middleware: [
+      LoggingMiddleware.printer(),
+      thunkMiddleware,
+    ],
   );
 
   runApp(MainApp(
